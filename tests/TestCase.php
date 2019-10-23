47fb2246ebdbad90b2724db2b317e38228029a54
Freddie Gar <freddie.gar@outlook.com>
2019-10-23 13:28:41 -0500

    Added test case

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FlexPHP\Inputs\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+/**
+ * Class TestCase
+ * @package FlexPHP\Inputs\Tests
+ */
+class TestCase extends PHPUnitTestCase
+{
+}
