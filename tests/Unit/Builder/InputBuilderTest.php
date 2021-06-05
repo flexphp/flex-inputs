763936ccdf3466458582a233d04fa70d21ad64c4
Freddie Gar <freddie.gar@outlook.com>
2021-06-05 03:48:08 -0500

    Updated dependencies

--- tests/Unit/Builder/InputBuilderTest.php
+++ tests/Unit/Builder/InputBuilderTest.php
@@ -281,7 +281,7 @@ T
         ]))->render();
 
         $this->assertEquals(<<<T
-<div class="form-group"><label for="form_foo">Foo</label>        <input type="range" id="form_foo" name="form[foo]" min="6" max="10" class="form-control" /></div>
+<div class="form-group"><label for="form_foo">Foo</label>        <input type="range" id="form_foo" name="form[foo]" min="6" max="10" class="form-control-range" /></div>
 T
 , $render);
     }
